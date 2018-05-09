4ff85deadb95c7f1c8a4e6e984dd42de0f92735f
zylo117 <zylo117@hotmail.com>
2018-05-09 17:03:35 +0800

    working on PDF bg cleaner

--- text_cleaner.py
+++ text_cleaner.py
@@ -8,18 +8,28 @@ class TextImg:
     # def remove_background(self):
 
     def _add_random_noise(self, white_noise=0, black_noise=0):
-        w, h = self.img.shape
+        h, w = self.img.shape
 
         white_noise_corrds = []
         black_noise_corrds = []
 
-        np.random.random_integers(white_noise)
+        white_noise_corrds_x = np.random.random_integers(w, size=(white_noise, 1))
+        white_noise_corrds_y = np.random.random_integers(h, size=(white_noise, 1))
+        white_noise_corrds = np.hstack([white_noise_corrds_y, white_noise_corrds_x])
+        self.img[white_noise_corrds] = 255
+
+        black_noise_corrds_x = np.random.random_integers(w, size=(black_noise, 1))
+        black_noise_corrds_y = np.random.random_integers(h, size=(black_noise, 1))
+        black_noise_corrds = np.hstack([black_noise_corrds_y, black_noise_corrds_x])
+        self.img[black_noise_corrds] = 0
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     text = TextImg("./test_chs.tiff")
 
     text._add_random_noise(white_noise=100, black_noise=100)
+    cv2.imshow(text.img)
+    cv2.waitKey(0)
 
     hist = cv2.calcHist([text.img], [0], None, [256], [0,256])
 
