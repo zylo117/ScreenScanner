c7c570c2e94c261056473dfb5ef227a185a8ae3d
zylo117 <zylo117@hotmail.com>
2018-05-10 11:27:55 +0800

    add sharpening

--- text_cleaner.py
+++ text_cleaner.py
@@ -2,6 +2,7 @@ import cv2
 import imutils
 import numpy as np
 
+
 class TextImg:
     def __init__(self, gray_img):
         self.img = gray_img
@@ -19,11 +20,28 @@ class TextImg:
         else:
             thresh_val, self.img = cv2.threshold(self.img, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_TOZERO)
 
+    def sharpen_text(self):
+        #kernel = np.array([[0, -1, 0], [1, 5, 1], [0, -1, 0]])
+        kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+        invert = cv2.bitwise_not(self.img).astype(np.uint16)
+        sharpen_mask = cv2.filter2D(invert, cv2.CV_64F, kernel, borderType=cv2.BORDER_REPLICATE)
+        self.img = cv2.convertScaleAbs(cv2.convertScaleAbs(sharpen_mask) + invert)
+        self.img = cv2.bitwise_not(self.img)
+
+    # hard core method
+    def refine_text(self):
+        kernel = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+        invert = cv2.bitwise_not(self.img).astype(np.uint16)
+        self.img = cv2.filter2D(invert, cv2.CV_64F, kernel, borderType=cv2.BORDER_REPLICATE)
+        M = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+        self.img = cv2.dilate(self.img, M)
+
+
     def _add_random_noise(self, white_noise=0, black_noise=0):
         h, w = self.img.shape[:2]
 
         white_noise_x = np.random.randint(0, w, size=(1, white_noise))
-        white_noise_y = np.random.randint(0, h, size=(1, white_noise ))
+        white_noise_y = np.random.randint(0, h, size=(1, white_noise))
         self.img[white_noise_y, white_noise_x] = np.random.randint(192, 256, size=white_noise)
 
         black_noise_x = np.random.randint(0, w, size=(1, black_noise))
@@ -44,12 +62,12 @@ class TextImg:
         else:
             angle = -angle
 
-        text_center = ((p1[0] + p3[0])//2,(p1[1] + p3[1])//2)
+        text_center = ((p1[0] + p3[0]) // 2, (p1[1] + p3[1]) // 2)
 
         M = cv2.getRotationMatrix2D(text_center, angle, 1)
 
         h, w = self.img.shape[:2]
-        self.img = cv2.warpAffine(self.img, M, (w, h), flags=cv2.INTER_LANCZOS4, borderMode=cv2.BORDER_CONSTANT, borderValue=255)
+        self.img = cv2.warpAffine(self.img, M, (w, h), flags=cv2.INTER_LANCZOS4, borderMode=cv2.BORDER_REPLICATE)
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
@@ -72,10 +90,17 @@ if __name__ == "__main__":
 
     text.skew_correction()
     cv2.imshow("Skew Correction", text.img)
+
+    # text.sharpen_text()
+    # cv2.imshow("Text Sharpening", text.img)
+
+    text.refine_text()
+    cv2.imshow("Text Refining", text.img)
     cv2.waitKey(0)
 
     text.img = imutils.resize(text.img, width=1000)
 
+    # show color distribution histogram
     hist = cv2.calcHist([text.img], [0], None, [256], [0, 256])
 
     plt.figure()
